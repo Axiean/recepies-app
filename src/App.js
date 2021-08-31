53eff434d89f5b67f3df9cca2871e61a95760f3e
Axiean <axieans@gmail.com>
2021-08-31 21:00:18 +0430

    completed base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/card/Card";
 
 function App() {
+  const [recipe, setRecipe] = useState([]);
+  const [search, setSearch] = useState("");
+  const [click, setClick] = useState(false);
+
+  const APP_ID = "5d67b3dd";
+  const APP_KEY = "af36dfcf99ac873c9364650204170f78";
+
+  useEffect(() => {
+    getData();
+  }, [click]);
+
+  const getData = async () => {
+    const Data = await fetch(
+      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const response = await Data.json();
+    setRecipe(response.hits);
+    console.log(response.hits);
+    setSearch("");
+  };
+  const clickHandler = (e) => {
+    e.preventDefault();
+    setClick(!click);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search_form" onSubmit={clickHandler}>
+        <input
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+          placeholder="Enter food name..."
+        />
+        <button type="submit">Get it!</button>
+      </form>
+      <ul>
+        {recipe.map((item) => {
+          return (
+            <Card
+              title={item.recipe.label}
+              image={item.recipe.image}
+              ingredientLines={item.recipe.ingredientLines}
+              mealType={item.recipe.mealType}
+              cuisineType={item.recipe.cuisineType}
+              calories={item.recipe.calories}
+              dishType={item.recipe.dishType}
+              key={item.recipe.label}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 }
---
+++ src/components/card/Card.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Card.css";
+import Swal from "sweetalert2";
+
+function Card({
+  title,
+  image,
+  recipe,
+  ingredientLines,
+  mealType,
+  cuisineType,
+  calories,
+  dishType,
+}) {
+  const integ = ingredientLines.join("<br />");
+  const clickHandler = async (e) => {
+    e.preventDefault();
+    await Swal.fire({
+      title: title,
+      html: integ,
+      width: 600,
+      padding: "3em",
+      background: "#fff url(/images/trees.png)",
+      backdrop: `
+      rgba(0,0,123,0.4)
+      url("/images/nyan-cat.gif")
+      left top
+      no-repeat
+    `,
+    });
+  };
+
+  return (
+    <div className="card">
+      <img src={image} alt={title} />
+      <div className="card_info">
+        <h3 className="mealtype">{mealType}</h3>
+        <h4 className="dishtype">{dishType}</h4>
+        <h2>{cuisineType}</h2>
+        <h1 className="title">{title}</h1>
+
+        {/* <ul>
+          {ingredientLines.map((item) => {
+            return <li>{item}</li>;
+          })}
+        </ul> */}
+
+        <h4 className="calories">Calories : {Math.floor(calories)}kcal </h4>
+        <button
+          onClick={clickHandler}
+          type="submit"
+          value={title}
+          data={ingredientLines}
+        >
+          Ingredients
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
